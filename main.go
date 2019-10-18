869277fd75df261e75e2f88601a339821ce60f98
Farizal Hamami <farizalhamami@gmail.com>
2019-10-18 17:16:22 +0700

    Create todo routes group

--- main.go
+++ main.go
@@ -15,11 +15,14 @@ func main() {
 	router := gin.Default()
 	v1 := router.Group("/v1")
 	{
-		v1.POST("/todo", controller.CreateTodo)
-		v1.GET("/todo", controller.GetAllTodo)
-		v1.GET("/todo/:id", controller.GetTodoByID)
-		v1.PUT("/todo/:id", controller.UpdateTodo)
-		v1.DELETE("/todo/:id", controller.DeleteTodo)
+		todo := v1.Group("/todo")
+		{
+			todo.POST("/", controller.CreateTodo)
+			todo.GET("/", controller.GetAllTodo)
+			todo.GET("/:id", controller.GetTodoByID)
+			todo.PUT("/:id", controller.UpdateTodo)
+			todo.DELETE("/:id", controller.DeleteTodo)
+		}
 	}
 	router.Run()
 }
